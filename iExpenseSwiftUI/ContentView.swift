10053191a38f903f742ba57b4727a894cf4a5319
Ibraheem <ibraheem.r@outlook.com>
2024-10-03 08:29:16 -0400

    Initial Commit

---
+++ iExpenseSwiftUI/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  iExpenseSwiftUI
+//
+//  Created by Ibraheem k. Rawlinson on 10/3/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ iExpenseSwiftUI/iExpenseSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  iExpenseSwiftUIApp.swift
+//  iExpenseSwiftUI
+//
+//  Created by Ibraheem k. Rawlinson on 10/3/24.
+//
+
+import SwiftUI
+
+@main
+struct iExpenseSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
